a749a0e65d29a4b2c5a755d074585071fe876fe3
an.taduychinh@hcmut.edu.vn <an.taduychinh@hcmut.edu.vn>
2024-04-21 00:25:14 +0700

    class chung to inherit

--- src/main/java/com/example/demo/object/Person.java
+++ src/main/java/com/example/demo/object/Person.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Person {
+    private String password;
+    private String username;
 
     private String name;
     private String age;
     private String location;
-
+    private String email;
+    private String phoneNumber;
+    private String sex;
+    private String cccd;
 
     public Person() {
         super();
@@ -37,5 +42,42 @@ public class Person {
     }
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getSex() {
+        return sex;
+    }
+    public String setSex(String sex){
+        this.sex = sex;
+    }
+    public String getCccd() {
+        return cccd;
+    }
+    public void setCccd(String cccd) {
+        this.cccd = cccd;
     }
 }
