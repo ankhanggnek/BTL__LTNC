01897885e95eb8ea13990fbd57afa421ed459543
an.taduychinh@hcmut.edu.vn <an.taduychinh@hcmut.edu.vn>
2024-04-21 00:28:30 +0700

    unfinished coding Login part

--- src/main/java/com/example/demo/RestdemoApplication.java
+++ src/main/java/com/example/demo/RestdemoApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
-
+import com.example.demo.Service.FirebaseService;
+import com.example.demo.objects.Person;
+import java.util.concurrent.ExecutionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RestdemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestdemoApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(RestdemoApplication.class, args);
+        FirebaseService firebaseService = context.getBean(FirebaseService.class);
     }
 
 }
