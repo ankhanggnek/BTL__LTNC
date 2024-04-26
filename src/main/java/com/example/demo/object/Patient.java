0b86a97e070f8aa07a5dc6170abfd195ac2a7353
Tong Duy Khang <tongduykhang01012004@gmail.com>
2024-04-26 22:00:53 +0700

    Them phuong thuc tu dong tao id cua Patient

--- src/main/java/com/example/demo/object/Patient.java
+++ src/main/java/com/example/demo/object/Patient.java
@@ -1,11 +1,18 @@
 package com.example.demo.object;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Getter
 @Setter
@@ -19,4 +26,21 @@ public class Patient extends Person {
     private PlanTreatment plane;
     private String date;
     private String roomCode;
+
+    public void setid() throws InterruptedException, ExecutionException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+        CollectionReference collection = dbFireStore.collection("Patient");
+        int serial = 1;
+        try {
+            Query query = collection.orderBy("serial", Query.Direction.DESCENDING).limit(1);
+            ApiFuture<QuerySnapshot> querySnapshot = query.get();
+            QuerySnapshot snapShot = querySnapshot.get();
+            if (!snapShot.isEmpty()) {
+                serial = snapShot.getDocuments().get(0).getLong("serial").intValue() + 1;
+            }
+        } catch (InterruptedException | ExecutionException e){
+            e.printStackTrace();
+        }
+        this.id = "BNNT" + serial;
+    }
 }
