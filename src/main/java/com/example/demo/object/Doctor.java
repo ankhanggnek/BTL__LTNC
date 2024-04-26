428b9e1f06f7e975370601195f90393da9b5fdcb
PoroDaSheep <164710579+PoroDaSheep@users.noreply.github.com>
2024-04-27 01:41:09 +0700

    Update Doctor.java

--- src/main/java/com/example/demo/object/Doctor.java
+++ src/main/java/com/example/demo/object/Doctor.java
@@ -1,10 +1,19 @@
 package com.example.demo.object;
 
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.stereotype.Component;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 
 @Component
 public class Doctor extends Person {
-
+    private int serial;
     private String id;
     private String department;
     private String qualifications;
@@ -15,8 +24,9 @@ public class Doctor extends Person {
         super();
     }
 
-    public Doctor(String name, String age, String location, String email, String phoneNumber, String sex, String cccd, String id, String department, String qualifications, boolean available, String time_off) {
-        super(name, age, location, email, phoneNumber, sex, cccd);
+    public Doctor(String name, String birthday, String location, String email, String phoneNumber, String sex, String cccd, int serial, String id, String department, String qualifications, boolean available, String time_off) {
+        super(name, birthday, location, email, phoneNumber, sex, cccd);
+        this.serial = serial;
         this.id = id;
         this.department = department;
         this.qualifications = qualifications;
@@ -24,14 +34,6 @@ public class Doctor extends Person {
         this.time_off = time_off;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }    
-    
     public String getDepartment() {
         return department;
     }
@@ -63,5 +65,23 @@ public class Doctor extends Person {
     public void setTime_off(String time_off) {
         this.time_off = time_off;
     }
-}
 
+    public void setid_serial() throws InterruptedException, ExecutionException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+        CollectionReference collection = dbFireStore.collection("Doctor");
+        int serial = 1;
+        try {
+            Query query = collection.orderBy("serial", Query.Direction.DESCENDING).limit(1);
+            ApiFuture<QuerySnapshot> querySnapshot = query.get();
+            QuerySnapshot snapShot = querySnapshot.get();
+            if (!snapShot.isEmpty()) {
+                serial = snapShot.getDocuments().get(0).getLong("serial").intValue() + 1;
+            }
+        } catch (InterruptedException | ExecutionException e){
+            e.printStackTrace();
+        }
+        this.id = "DOCTOR" + serial;
+        this.serial = serial;
+    }
+
+}
